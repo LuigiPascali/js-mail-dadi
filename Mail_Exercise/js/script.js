c09d9b07a10304fe97559e292642a9a68c2793fc
luigimariapascali <luigimariapascali@gmail.com>
2023-06-15 15:37:58 +0200

    scritto programma in forma testuale

---
+++ Mail_Exercise/js/script.js
@@ -0,0 +1,18 @@
+/* 
+    Creo una lista degli indirizzi email consentiti
+
+    Richiedo di inserire la mail, facendo apparire a schermo un prompt
+        SE non vengono inseriti "@" o ".com" appare un alert con scritto "l'indirizzo mail non è valido"
+        ALTRIMENTI eseguo un ciclo per controllare se l'indirizzo email è presente nella lista
+    
+    Ciclo controllo presenza lista mail
+        SE l'inidirizzo mail è presente nella lista
+            ALLORA parte un alert con scritto "Accesso Consentito"
+        ALTRIMENTI 
+            ALLORA parte un alert con scritto "Accesso Non Autorizzato"
+*/
+
+
+
+
+
